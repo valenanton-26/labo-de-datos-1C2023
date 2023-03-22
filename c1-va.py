ec4f336d971c10e8862a3fd540cf384fd17a5cf3
valenanton-26 <81269848+valenanton-26@users.noreply.github.com>
2023-03-22 09:38:05 -0300

    Update c1-va.py

--- c1-va.py
+++ c1-va.py
@@ -35,3 +35,21 @@ while rebotes <= 10:
     altura = altura * 3/5
     print(rebotes , altura)
     rebotes +=1
+    
+
+# traducir las palabras masculinas a e
+frase = 'todos somos programadores'
+palabras = frase.split()
+frase_t = ""
+for palabra in palabras: 
+    palabra_t = ""
+    if palabra[-1] == "o":
+        palabra_t = palabra[:-1] + "e"
+    elif palabra[-2] == "o":
+        palabra_t = palabra[:-2] + "e" + palabra[-1]
+    else:
+        palabra_t = palabra
+    
+    frase_t += palabra_t + " "        
+
+print(frase_t)
